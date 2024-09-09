9c78f090b625f12dcdfed90c4e8fb55138adea40
Furkan Gunes <furkanguness2003@gmail.com>
2024-09-09 17:30:26 +0300

    uptaded

--- app/src/main/java/com/example/javadersleri/degiskenler/DegiskenOlusturma.java
+++ app/src/main/java/com/example/javadersleri/degiskenler/DegiskenOlusturma.java
@@ -1,4 +1,29 @@
 package com.example.javadersleri.degiskenler;
 
 public class DegiskenOlusturma {
+    public static void main(String[] args) {
+        String ogrenciAdi = "Ahmet";
+        int ogrenciYas = 23;
+        double ogrenciBoy = 1.78;
+        char ogrenciBasharf = 'A';
+        boolean ogrenciDevamEdiyorMu = true;
+
+        System.out.println(ogrenciAdi);
+        System.out.println(ogrenciYas);
+        System.out.println(ogrenciBoy);
+        System.out.println(ogrenciBasharf);
+        System.out.println(ogrenciDevamEdiyorMu);
+
+        int urun_id = 3416;
+        String urun_adi = "Kol saati";
+        int urun_adet = 100;
+        float urun_fiyat = 149.99f;
+        String urun_tedarikci = "Rolex";
+
+        System.out.println("Ürün id        : " + urun_id);
+        System.out.println("Ürün adı       : " + urun_adi);
+        System.out.println("Ürün adet      : " + urun_adet);
+        System.out.println("Ürün fiyat     : " + urun_fiyat + " ₺");
+        System.out.println("Ürün tedarikçi : " + urun_tedarikci);
+    }
 }
--- app/src/main/java/com/example/javadersleri/degiskenler/TurDonusumu.java
+++ app/src/main/java/com/example/javadersleri/degiskenler/TurDonusumu.java
@@ -2,6 +2,29 @@ package com.example.javadersleri.degiskenler;
 
 public class TurDonusumu {
     public static void main(String[] args) {
+        //Tür Dönüşümü - Type Casting
+        //Sayısaldan sayısala
+        int i = 67;
+        double d = 45.78;
 
+        int sonuc1 = (int) d;//explicit : açıklamalı, büyüğü küçüğe dönüştürürken açıklama olacak.
+        double sonuc2 = i;//unexplicit : açıklama olmadan
+        System.out.println(sonuc1);
+        System.out.println(sonuc2);
+
+        //Sayısaldan Metine
+        String sonuc3 = String.valueOf(d);//"45.78"
+        String sonuc4 = String.valueOf(i);//"67"
+        System.out.println(sonuc3);
+        System.out.println(sonuc4);
+
+        //Metinden sayısala
+        String yazi1 = "56";//int
+        String yazi2 = "23.87";//double
+
+        int sonuc5 = Integer.parseInt(yazi1);
+        double sonuc6 = Double.parseDouble(yazi2);
+        System.out.println(sonuc5);
+        System.out.println(sonuc6);
     }
 }
